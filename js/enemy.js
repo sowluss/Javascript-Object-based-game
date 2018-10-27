e8297a365dba95a460c7c2098a2b7c6c7ce56687
sowluss <sowluss@gmail.com>
2018-10-27 23:57:14 +0300

    JS object practice based on a game, added relevant files

---
+++ js/enemy.js
@@ -0,0 +1,11 @@
+let enemy;
+
+function Enemy(enemyType, health, mana, strenght, agility, speed) {
+// check the below
+  this.enemyType = enemyType;
+  this.health = health;
+  this.mana = mana;
+  this.strenght = strenght;
+  this.agility = agility;
+  this.speed = speed;
+}
---
+++ js/player.js
@@ -0,0 +1,12 @@
+let player;
+
+function Player(classType, health, mana, strenght, agility, speed) {
+  this.classType = classType;
+  this.health = health;
+  this.mana = mana;
+  this.strenght = strenght;
+  this.agility = agility;
+  this.speed = speed;
+}
+
+let player = new Player();
